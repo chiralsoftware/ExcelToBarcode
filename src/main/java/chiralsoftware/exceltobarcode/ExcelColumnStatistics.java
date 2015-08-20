a716035acad7261020028dda7ed5ed249253e3cb
Eric Hollander <eric@chiralsoftware.com>
2015-08-20 13:44:40 -0700

    updated versions

--- src/main/java/chiralsoftware/exceltobarcode/ExcelColumnStatistics.java
+++ src/main/java/chiralsoftware/exceltobarcode/ExcelColumnStatistics.java
@@ -1,10 +1,5 @@
 package chiralsoftware.exceltobarcode;
 
-import au.com.bytecode.opencsv.CSVReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,7 +7,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import org.apache.poi.ss.usermodel.Cell;
